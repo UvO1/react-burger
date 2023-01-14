d0f14b20881d926274e938b73ec2ac9b178bd3cd
Ольга <olga@MacBook-Air-Olga.local>
2023-01-15 00:59:57 +0100

    with moduls just render

--- src/components/ingredient-details/ingredient-details.js
+++ src/components/ingredient-details/ingredient-details.js
@@ -10,7 +10,20 @@ function IngredientDetails(){
             <p className={`${IngredientDetailsStyles.title} text text_type_main-medium mt-4`}>Биокотлета из марсианской Магнолии</p>
             <div className={`${IngredientDetailsStyles.info} mt-8`}>
                 <div className={IngredientDetailsStyles.item}>
-
+                    <p className="text text_type_main-default">Калории, ккал</p>
+                    <p className="text text_type_digits-default">4242</p>
+                </div>
+                <div className={IngredientDetailsStyles.item}>
+                    <p className="text text_type_main-default">Белки, г</p>
+                    <p className="text text_type_digits-default">420</p>
+                </div>
+                <div className={IngredientDetailsStyles.item}>
+                    <p className="text text_type_main-default">Жиры, г</p>
+                    <p className="text text_type_digits-default">142</p>
+                </div>
+                <div className={IngredientDetailsStyles.item}>
+                    <p className="text text_type_main-default">Углеводы, г</p>
+                    <p className="text text_type_digits-default">242</p>
                 </div>
             </div>
         </div>
